dc0cb287544f822929747ba56332c4c43fa38aa9
Pancake <nik.moskovenko@yandex.ru>
2025-03-02 17:38:26 +0300

    remove CompilationEvent

--- main-service/src/main/java/ru/practicum/main_service/compilation/repository/CompilationEventsRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.main_service.compilation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.main_service.compilation.model.CompilationEvent;
-import ru.practicum.main_service.compilation.model.CompilationEventKey;
-
-public interface CompilationEventsRepository extends JpaRepository<CompilationEvent, CompilationEventKey> {
-}
