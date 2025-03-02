c0fa96004aed1fbaf3e1ae21d165d3ae497d93e3
Pancake <nik.moskovenko@yandex.ru>
2025-03-02 17:41:27 +0300

    implement CompilationService

--- main-service/src/main/java/ru/practicum/main_service/compilation/service/CompilationService.java
+++ main-service/src/main/java/ru/practicum/main_service/compilation/service/CompilationService.java
@@ -1,11 +1,24 @@
 package ru.practicum.main_service.compilation.service;
 
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.main_service.compilation.model.Compilation;
+import ru.practicum.main_service.compilation.dto.CompilationDto;
+import ru.practicum.main_service.compilation.dto.NewCompilationDto;
+import ru.practicum.main_service.compilation.dto.UpdateCompilationDto;
 
 import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CompilationService {
-    List<Compilation> getAll();
+
+    List<CompilationDto> getCompilations(Boolean pinned, int from, int size);
+
+    CompilationDto getById(int id);
+
+    @Transactional
+    CompilationDto add(NewCompilationDto dto);
+
+    @Transactional
+    CompilationDto update(int id, UpdateCompilationDto dto);
+
+    void delete(int id);
 }
